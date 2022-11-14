dd7f38db526640e8074c5c0ee88d4e479d2981b4
Dmitry Kuznetsov <115784315+DmitryKuznetsov83@users.noreply.github.com>
2022-11-14 15:00:50 +0200

    Update DepositCalculate.java

--- src/DepositCalculate.java
+++ src/DepositCalculate.java
@@ -4,19 +4,19 @@ public class DepositCalculate {
     double CalculateComplexPercent(double deposit, double percent, int depositPeriod) {
         double pay = deposit * Math.pow((1 + percent / 12), 12 * depositPeriod);
         return getRound(pay, 2);
-    }
+    } // возможно имеет смысл сделать отступ перед объявлением месяца
 
     double CalculateSimplePercent(double doubleAmount, double doubleYearRate, int depositPeriod) {
         return getRound(doubleAmount + doubleAmount * doubleYearRate * depositPeriod, 2);
-    }
+    } // тут все отлично
 
     double getRound(double value, int places) {
         double ScaLe = Math.pow(10, places);
         return Math.round(value * ScaLe) / ScaLe;
-    }
+    } // тут все отлично
 
     void TheSurvey() {
-        int period, action, amount;
+        int period, action, amount; // ты забыл, что каждое поле нужно объявлять отдельно
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Введите сумму вклада в рублях:");
@@ -37,7 +37,7 @@ public class DepositCalculate {
             out = CalculateSimplePercent(amount, 0.06, period);
         }
         System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + out);
-    }
+    } // все супере, кроме стр.19
 
     public static void main(String[] args) {
         new DepositCalculate().TheSurvey();
